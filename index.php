6e0b1d239a5c13940f50a5d9024f18c67ecc48c2
raffaele-catalano <raf.catalano@gmail.com>
2023-05-17 18:10:35 +0200

    implemented basic markup + some arrays + style + favicon

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+
+// array di lettere minuscole
+$letters        = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// array di lettere maiuscole 
+$lettersCaps    = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+// array di numeri
+$numbers    = ['0','1','2','3','4','5','6','7','8','9'];
+// array di simboli
+$symbols = ['!','?','&','$','£','%','/','=','+','-','_','^','@','#','*'];
+
+
+var_dump($letters, $lettersCaps, $numbers, $symbols);
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css' integrity='sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==' crossorigin='anonymous'/>
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="assets/img/pw_favicon.png" type="png">
+    <!-- css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Strong Password Generator</title>
+</head>
+
+<body>
+    <div class="wrapper bg-dark text-light">
+    <h1>test</h1>
+    </div>
+</body>
+
+</html>
